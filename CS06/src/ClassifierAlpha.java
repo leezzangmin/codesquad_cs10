875c1446780a58135c401c30bf28052a6829126e
leezzangmin <64303390+leezzangmin@users.noreply.github.com>
2022-01-20 22:44:41 +0900

    람다, 스트림

---
+++ CS06/src/ClassifierAlpha.java
@@ -0,0 +1,61 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.Predicate;
+
+public class ClassifierAlpha {
+    private int number;
+
+    public ClassifierAlpha(int number) {
+        this.number = number;
+    }
+
+    Predicate<Integer> isFactor = i -> this.number % i == 0;
+
+    public Set factors(int target) { // supplier?
+        HashSet factors = new HashSet<>();
+        for (int pod = 1 ; pod < target ; pod++) {
+            if (isFactor.test(pod)) {
+                factors.add(pod);
+            }
+        }
+        return factors;
+    }
+
+    static public int sum(Set factors) {
+        return factors.parallelStream().mapToInt(i -> (int)i).sum();
+    }
+
+    public ArrayList<String> defineNumber() {
+        ArrayList<String> answer = new ArrayList<String>(4);
+        if(isPerfect.test(this.number))
+            answer.add("perfect");
+        else if(isAbundant.test(this.number))
+            answer.add("abundant");
+        else
+            answer.add("deficient");
+        if(isPrime.test(this.number))
+            answer.add("prime");
+        if(isSquared.test(this.number))
+            answer.add("squared");
+        return answer;
+    }
+
+    Predicate<Integer> isPerfect = i -> sum(factors(i)) == i ? true : false;
+    Predicate<Integer> isAbundant = i -> sum(factors(i)) > i ? true : false;
+    Predicate<Integer> isDeficient = i -> sum(factors(i)) < i ? true : false;
+    Predicate<Integer> isPrime = i -> sum(factors(i))+this.number-1 == i ? true : false;
+    Predicate<Integer> isSquared = i -> Math.sqrt(i) % 1 == 0 ? true : false; // 어떤 자연수의 제곱이 되는 수
+
+    public static void main(String[] args) {
+        for(int i=2;i<=100;i++){
+            ClassifierAlpha a = new ClassifierAlpha(i);
+            System.out.print(i);
+            System.out.println(a.defineNumber());
+
+        }
+
+    }
+}
+//factors.stream().forEach(System.out::println);
---
+++ CS06/src/PrimeAlpha.java
@@ -0,0 +1,40 @@
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class PrimeAlpha {
+    private int number;
+
+    public PrimeAlpha(int number) {
+        this.number = number;
+    }
+
+    public boolean isPrime() {
+        Set primeSet = new HashSet(){ {add(1); add(number);} };
+        return number > 1 && factors().equals(primeSet);
+    }
+
+    public boolean isFactor(int potentialFactor) {
+        return number % potentialFactor == 0;
+    }
+
+    public Set factors() {
+        HashSet factors = new HashSet<>();
+        for (int pod=1; pod <= Math.sqrt(number); pod++) {
+            if (isFactor(pod)) {
+                factors.add(pod);
+                factors.add(number / pod);
+            }
+        }
+        return factors;
+    }
+
+    public static void main(String[] args) {
+        PrimeAlpha prime1 = new PrimeAlpha(10);
+        PrimeAlpha prime2 = new PrimeAlpha(7);
+
+        //System.out.println(prime1.isPrime());
+        //System.out.println(prime2.isPrime());
+        System.out.println(1.1%1);
+    }
+}
