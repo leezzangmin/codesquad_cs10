f1d757d6c85612b814b3eec6e4753c8eec17d70c
leezzangmin <64303390+leezzangmin@users.noreply.github.com>
2022-02-09 15:33:29 +0900

    3번째 명세 완료 + test

--- pair_programming/src/MAIN.java
+++ pair_programming/src/MAIN.java
@@ -2,6 +2,19 @@ public class MAIN {
     public static void main(String[] args){
         MyCalendar myCalendar = new MyCalendar();
 
-        System.out.println(myCalendar.now);
+        System.out.println(myCalendar.getLocalDateTime("2020-01-01"));
+        try {
+            System.out.println(myCalendar.getLocalDateTime("2020-13-01"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(myCalendar.getLocalDateTime(2020, 1, 1));
+        try {
+            System.out.println(myCalendar.getLocalDateTime(2020, 13, 1));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
--- pair_programming/src/MyCalendar.java
+++ pair_programming/src/MyCalendar.java
@@ -1,7 +1,25 @@
 import java.time.LocalDateTime;
 
 public class MyCalendar {
-    LocalDateTime now = LocalDateTime.now();
+    // LocalDateTime now = LocalDateTime.now();
 
-//    public
+    public DateData getDateDataNow() {
+        LocalDateTime now = LocalDateTime.now();
+        return new DateData(now);
+    }
+
+    public LocalDateTime getLocalDateTime(String date) {
+        String[] dateArray = date.split("-");
+
+        int year = Integer.parseInt(dateArray[0]);
+        int month = Integer.parseInt(dateArray[1]);
+        int day = Integer.parseInt(dateArray[2]);
+
+        return LocalDateTime.of(year, month, day, 0, 0, 0);
+    }
+
+    public LocalDateTime getLocalDateTime(int year, int month, int day) {
+        return LocalDateTime.of(year, month, day, 0, 0, 0);
+    }
+
 }
